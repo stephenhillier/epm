c0cb723e3168898dd23510e20d8bf24bbed88c44
Steve <stephenhillier@gmail.com>
2017-09-21 14:14:54 -0700

    Register SoilSample model

--- projects/admin.py
+++ projects/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Project, DataPoint, SoilLayer
+from .models import Project, DataPoint, SoilLayer, SoilSample
 
 # Register your models here.
 admin.site.register(Project)
 admin.site.register(DataPoint)
 admin.site.register(SoilLayer)
+admin.site.register(SoilSample)
