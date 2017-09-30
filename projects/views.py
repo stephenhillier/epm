18da14aa5cdf4ddefd6dd57498d29d29c00454ce
Steve <stephenhillier@gmail.com>
2017-09-29 17:43:52 -0700

    Removing unused views

--- projects/views.py
+++ projects/views.py
@@ -1,74 +1,9 @@
-from django.views.generic import TemplateView, DetailView, ListView, CreateView
+from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from rest_framework import permissions, viewsets
 from rest_framework.reverse import reverse
 from projects.models import Project, DataPoint, SoilLayer, SoilSample
 from projects.serializers import ProjectSerializer, UserSerializer, DataPointSerializer, SoilLayerSerializer
-
-
-class ProjectHomeView(LoginRequiredMixin, TemplateView):
-    template_name = 'projects/start.html'
-
-
-class ProjectsListView(LoginRequiredMixin, ListView):
-    model = Project
-
-
-class ProjectDetailView(LoginRequiredMixin, DetailView):
-    model = Project
-    pk_url_kwarg = 'project'
-
-
-class ProjectCreateView(LoginRequiredMixin, CreateView):
-    model = Project
-    fields = ['number', 'name', 'pm', 'location', 'client']
-
-
-class InstrumentList(LoginRequiredMixin, DetailView):
-    template_name = 'projects/instrument_list.html'
-    model = Project
-    pk_url_kwarg = 'project'
-
-
-class InstrumentDetailView(LoginRequiredMixin, DetailView):
-    """
-    Instrument detail view
-    - Expects <project> and <datapoint> in url kwargs
-    """
-    template_name = 'projects/instrument_detail.html'
-    model = Project
-    pk_url_kwarg = 'project'
-
-    # Add requested instrument to context:
-    def get_context_data(self, **kwargs):
-        context = super(InstrumentDetailView, self).get_context_data(**kwargs)
-        instrument = DataPoint.objects.get(pk=self.kwargs['datapoint'])
-        context['instrument'] = instrument
-        return context
-
-
-class TestHoleList(LoginRequiredMixin, DetailView):
-    template_name = 'projects/testhole_list.html'
-    model = Project
-    pk_url_kwarg = 'project'
-
-
-class TestHoleDetailView(LoginRequiredMixin, DetailView):
-    """
-    Testhole detail view
-    - Expects <project> and <datapoint> in url kwargs
-    """
-    template_name = 'projects/testhole_detail.html'
-    model = Project
-    pk_url_kwarg = 'project'
-
-    # Add requested testhole to context:
-    def get_context_data(self, **kwargs):
-        context = super(TestHoleDetailView, self).get_context_data(**kwargs)
-        testhole = DataPoint.objects.get(pk=self.kwargs['datapoint'])
-        context['testhole'] = testhole
-        return context
-
 
 class APIProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
@@ -98,3 +33,7 @@ class APISoilLayerViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(datapoint_id=self.kwargs['datapoint'])
+
+
+class ProjectsHomeView(LoginRequiredMixin, views.TemplateView):
+    template = "projects/base.html"
