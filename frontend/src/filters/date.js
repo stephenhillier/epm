8c080479ef235810e93e5ff47a5a2d9b40e8ea4c
Steve <stephenhillier@gmail.com>
2017-10-02 14:48:23 -0700

    removing old frontend files

--- frontend/src/filters/date.js
+++
@@ -1,4 +0,0 @@
-export default (value) => {
-  const date = new Date(value)
-  return date.toLocaleString(['en-us'], { month: 'short', day: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit' })
-}
