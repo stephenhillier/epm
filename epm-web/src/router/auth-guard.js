6c0658570aaac3001245bd304015d32bb105ff75
Steve <stephenhillier@gmail.com>
2017-12-06 15:42:52 -0800

    changed router guard to delete token if expired

--- epm-web/src/router/auth-guard.js
+++ epm-web/src/router/auth-guard.js
@@ -4,10 +4,19 @@ import axios from 'axios'
 export default (to, from, next) => {
   var ts = Math.round((new Date()).getTime() / 1000)
   var token = localStorage.getItem('token')
-  if (token && localStorage.getItem('tokenExpiry') > ts && localStorage.getItem('user')) {
-    store.dispatch('changeUser', localStorage.getItem('user'))
-    axios.defaults.headers.common = { 'Authorization': 'JWT ' + token }
-    next()
+  var expiry = localStorage.getItem('tokenExpiry')
+  var user = localStorage.getItem('user')
+  if (token && user) {
+    if (expiry > ts) {
+      store.dispatch('changeUser', localStorage.getItem('user'))
+      axios.defaults.headers.common = { 'Authorization': 'JWT ' + token }
+      next()
+    } else {
+      localStorage.removeItem('token')
+      localStorage.removeItem('user')
+      localStorage.removeItem('tokenExpiry')
+      next('/login')
+    }
   } else {
     next('/login')
   }
