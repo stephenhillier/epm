b99f8b43137b8c0dd2a2df6b8ffa85c52b34254a
Steve <stephenhillier@gmail.com>
2017-12-24 11:02:03 -0800

    Changed order of state keys and actions and added comments

--- epm-web/src/store/index.js
+++ epm-web/src/store/index.js
@@ -3,39 +3,56 @@ import Vuex from 'vuex'
 import axios from 'axios'
 import router from '../router'
 
+/* API urls */
 const api = 'https://www.earthworksqc.com/projects/api'
 const loginApi = 'https://www.earthworksqc.com/auth'
 
 Vue.use(Vuex)
 
+/**
+ * Vuex state store for retrieving and storing incoming data from API.
+ *
+ * TODO: separate state, mutations, actions and getters into modules e.g.:
+ *
+ * Projects:
+ *  state:
+ *   currentProject
+ *  mutations:
+ *    changeCurrentProject ()
+ *  actions:
+ *    loadCurrentProjectData ()
+ *  getters:
+ *   getCurrentProjectData ()
+ */
+
 export const store = new Vuex.Store({
   state: {
+    currentUser: null,
+
+    // helper states for displaying loading and error messages
     loading: false,
     error: '',
-    currentUser: null,
+    message: null,
+
+    // Project list and currently selected project
     myProjects: [],
     currentProject: {},
-    message: null,
     projectData: [],
-    datatypeOptions: [],
+
+    // Data item detail pages
     currentBorehole: { location: { latitude: 0, longitude: 0 } },
+    currentInstrument: {},
     projectSamples: [],
-    uscsOptions: [{ value: '', display_name: '' }],
     sampleList: [],
-    currentInstrument: {}
+
+    // form select options
+    uscsOptions: [{ value: '', display_name: '' }],
+    datatypeOptions: []
   },
   mutations: {
-    loadRetrievedProjects (state, payload) {
-      state.myProjects = payload
-    },
+    // Session state mutations
     setUser (state, payload) {
       state.currentUser = payload
-    },
-    setProjectData (state, payload) {
-      state.projectData = payload
-    },
-    setLoading (state, payload) {
-      state.loading = payload
     },
     setError (state, payload) {
       state.error = payload
@@ -43,35 +60,58 @@ export const store = new Vuex.Store({
     clearError (state) {
       state.error = ''
     },
-    setCurrentProject (state, payload) {
-      state.currentProject = payload
-    },
     setMessage (state, payload) {
       state.message = payload
     },
     clearMessage (state) {
       state.message = null
     },
-    setDatatypeOptions (state, payload) {
-      state.datatypeOptions = payload
+    setLoading (state, payload) {
+      state.loading = payload
     },
+
+    // Projects state mutations
+    loadRetrievedProjects (state, payload) {
+      state.myProjects = payload
+    },
+    setCurrentProject (state, payload) {
+      state.currentProject = payload
+    },
+    setProjectData (state, payload) {
+      state.projectData = payload
+    },
+
+    // Borehole/Instrument/Samples state mutations
     setCurrentBorehole (state, payload) {
       state.currentBorehole = payload
     },
     setCurrentInstrument (state, payload) {
       state.currentInstrument = payload
     },
-    setUscsOptions (state, payload) {
-      state.uscsOptions = payload
-    },
     setSampleList (state, payload) {
       state.sampleList = payload
+    },
+
+    // fill in data for input form select boxes
+    setDatatypeOptions (state, payload) {
+      state.datatypeOptions = payload
+    },
+    setUscsOptions (state, payload) {
+      state.uscsOptions = payload
     }
   },
   actions: {
-    resetError ({commit}) {
-      commit('clearError')
-    },
+    /**
+     * Session/user/login actions
+     *
+     * userLogin: makes POST request to get token from API
+     * userLogout: makes POST request to logout, clears tokens
+     * changeUser: changes name of user in state. No API call
+     *  - normally sets either null or user's name (on login)
+     *  - i.e. doesn't change from one user to another
+     * resetError: resets error message
+     *
+     */
     userLogin ({commit}, credentials) {
       commit('clearError')
       axios.post(loginApi + '/login/', credentials)
@@ -82,11 +122,16 @@ export const store = new Vuex.Store({
         console.log('User: ' + username + ', token: ' + token)
         localStorage.setItem('user', username)
         localStorage.setItem('token', token)
+
+        // decode JWT token to capture expiry
         const base64Url = token.split('.')[1]
         const base64 = base64Url.replace('-', '+').replace('_', '/')
         const exp = JSON.parse(window.atob(base64)).exp
         localStorage.setItem('tokenExpiry', exp)
+
+        // Add token to headers when making API calls with axios
         axios.defaults.headers.common = { 'Authorization': 'JWT ' + token }
+
         this.dispatch('loadProjects')
         router.push('/')
       })
@@ -103,6 +148,24 @@ export const store = new Vuex.Store({
       commit('setUser', null)
       router.push('/login')
     },
+    changeUser ({commit}, payload) {
+      commit('setUser', payload)
+    },
+    resetError ({commit}) {
+      commit('clearError')
+    },
+
+    /**
+     * Project actions
+     *
+     * loadProjects: retrieves list of projects with GET request
+     * loadCurrentProject: retrieves details from selected project (GET)
+     * loadProjectData: retrieves available data from selected project (GET)
+     *  e.g. boreholes, instruments, samples etc.
+     * clearCurrentProject: clears the current project from state
+     *  e.g. when leaving project view
+     * addNewProject: adds a new project to database with POST api request
+     * */
     loadProjects ({commit}) {
       commit('setLoading', true)
       axios.get(api + '/projects/')
@@ -110,6 +173,10 @@ export const store = new Vuex.Store({
           const projects = []
           const obj = data.data
           for (let item in obj) {
+            // make objects out of response data, store in array, send to state
+            // todo: can possibly use response data array directly (with serializer)
+            // also avoid (let item in arr) for iterating through array
+            // (to fix when ready to test)
             projects.push({
               id: obj[item].id,
               number: obj[item].number,
@@ -174,6 +241,7 @@ export const store = new Vuex.Store({
     },
     addNewProject ({commit}, payload) {
       commit('setLoading', true)
+      // API accepts object with keys name (project name), number (proj. number), client, location
       const project = {
         name: payload.name,
         number: payload.number,
@@ -195,6 +263,18 @@ export const store = new Vuex.Store({
         }
       )
     },
+
+    /**
+     * Data (Borehole/Instrument/Sample) actions
+     *
+     * loadBoreholeData: retrieves data for the current borehole with GET request
+     * loadInstrumentData: retrieves data for current instrument with GET request
+     * loadSampleData: retrieves data for current sample with GET request
+     *
+     * addDatapoint: sends a new datapoint (borehole or instrument) with POST
+     * addSoilLayer: creates a new soil layer in a borehole object (POST)
+     * addSoilSample: creates a new soil sample in a borehole (POST)
+     * */
     loadBoreholeData ({commit}, payload) {
       commit('setLoading', true)
       axios.get(api + '/projects/' + payload.id + '/data/' + payload.bh + '/')
@@ -224,55 +304,6 @@ export const store = new Vuex.Store({
       .catch(
         error => {
           commit('setLoading', false)
-          commit('setError', error)
-          console.log(error)
-        }
-      )
-    },
-    changeUser ({commit}, payload) {
-      commit('setUser', payload)
-    },
-    loadDatatypeOptions ({commit}, projectId) {
-      commit('setLoading', true)
-      axios.options(api + '/projects/' + projectId + '/data/')
-      .then(
-        response => {
-          const datatypes = response.data.actions.POST.data_type.choices
-          const options = []
-          for (let item in datatypes) {
-            options.push({
-              value: datatypes[item].value,
-              display: datatypes[item].display_name
-            })
-          }
-          commit('setLoading', false)
-          commit('setDatatypeOptions', options)
-        }
-      )
-      .catch(
-        error => {
-          commit('setLoading', false)
-          commit('setError', error)
-          console.log(error)
-        }
-      )
-    },
-    loadUscsOptions ({commit}, payload) {
-      axios.options(api + '/projects/' + payload.project_id + '/data/' + payload.borehole_id + '/soil_layers/')
-      .then(
-        response => {
-          const uscsChoices = response.data.actions.POST.uscs.choices
-          const choices = []
-          for (let item in uscsChoices) {
-            choices.push({
-              value: uscsChoices[item].value,
-              display_name: uscsChoices[item].display_name
-            })
-          }
-          commit('setUscsOptions', choices)
-        })
-      .catch(
-        error => {
           commit('setError', error)
           console.log(error)
         }
@@ -345,6 +376,62 @@ export const store = new Vuex.Store({
             }
           }
           commit('setSampleList', sampleList)
+        }
+      )
+    },
+
+    /**
+     * Form select field actions (populates select fields in create forms)
+     *
+     * loadDataTypeOptions: makes options request and receives list of valid instrument types
+     *  e.g. Borehole, Settlement gauge, Inclinometer
+     *
+     * loadUscsOptions: makes options request and receives valid soil classifications
+     *  e.g. GW, SP-SM, ML
+     */
+    loadDatatypeOptions ({commit}, projectId) {
+      commit('setLoading', true)
+      axios.options(api + '/projects/' + projectId + '/data/')
+      .then(
+        response => {
+          const datatypes = response.data.actions.POST.data_type.choices
+          const options = []
+          for (let item in datatypes) {
+            options.push({
+              value: datatypes[item].value,
+              display: datatypes[item].display_name
+            })
+          }
+          commit('setLoading', false)
+          commit('setDatatypeOptions', options)
+        }
+      )
+      .catch(
+        error => {
+          commit('setLoading', false)
+          commit('setError', error)
+          console.log(error)
+        }
+      )
+    },
+    loadUscsOptions ({commit}, payload) {
+      axios.options(api + '/projects/' + payload.project_id + '/data/' + payload.borehole_id + '/soil_layers/')
+      .then(
+        response => {
+          const uscsChoices = response.data.actions.POST.uscs.choices
+          const choices = []
+          for (let item in uscsChoices) {
+            choices.push({
+              value: uscsChoices[item].value,
+              display_name: uscsChoices[item].display_name
+            })
+          }
+          commit('setUscsOptions', choices)
+        })
+      .catch(
+        error => {
+          commit('setError', error)
+          console.log(error)
         }
       )
     }
